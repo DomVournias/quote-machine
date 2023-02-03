96c40d17329250819e4ae8fc5f21fe085ccf0d8f
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2023-02-03 02:45:48 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [quotes, setQuotes] = useState({});
+  const [quote, setQuote] = useState({
+    text: "",
+    author: "",
+  });
+
+  const quotesLink =
+    "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json";
+
+  function onLoadQuote() {
+    fetch(quotesLink)
+      .then((res) => res.json())
+      .then((data) => setQuotes(data.quotes));
+  }
+
+  function randomQuote() {
+    if (quotes.length >= 0) {
+      const randomIndex = Math.floor(Math.random() * quotes.length);
+      const randomQuote = quotes.splice(randomIndex, 1)[0];
+      setQuote({
+        ...quote,
+        text: randomQuote.quote,
+        author: randomQuote.author,
+      });
+    }
+  }
+
+  const handleNextQuote = () => {
+    randomQuote();
+  };
+
+  useEffect(() => {
+    onLoadQuote();
+  }, []);
+
+  useEffect(() => {
+    randomQuote();
+  }, [quotes]);
+
+  const twitterShareLink = `https://twitter.com/intent/tweet?hashtags=quotes&related=domvournias&text=${`${quote.text} ~ ${quote.author}`}`;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="wrapper">
+      <div id="quote-box">
+        <div className="quote">
+          <div id="text">{quote.text}</div>
+          <div id="author">~ {quote.author}</div>
+        </div>
+        <div className="row">
+          <button onClick={handleNextQuote} id="new-quote">
+            New quote
+          </button>
+          <a
+            id="tweet-quote"
+            href={twitterShareLink}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Share it
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
